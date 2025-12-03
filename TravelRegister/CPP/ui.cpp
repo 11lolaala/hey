6a815fc896fda1801e90ff97d136286400fc00c1
11lolaala <mishagrigoriev1710@gmail.com>
2025-12-03 22:35:16 +0200

    Add files via upload

---
+++ TravelRegister/CPP/ui.cpp
@@ -0,0 +1,14 @@
+#include "../HPP/ui.hpp"
+#include <iostream>
+#include <string>
+
+void menu(){
+    std::cout<<"Выберите комнаду:"<<std::endl
+    <<"1. Добавить командировку"<<std::endl
+    <<"2. Просмотреть все командировки"<<std::endl
+    <<"3. Найти по ФИО"<<std::endl
+    <<"4. Добавить срочную командировку"<<std::endl
+    <<"0. Выход"<<std::endl;
+}
+
+//floatb double чем отличается
